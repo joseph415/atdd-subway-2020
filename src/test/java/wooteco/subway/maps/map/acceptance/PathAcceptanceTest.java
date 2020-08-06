29b34a02189922ac159272a33c855914e7f3cda4
Eun Seok <jeseok95@gmail.com>
2020-08-06 21:49:32 +0900

    refactor: PathAcceptanceTest 통과하도록 변경
    - 로그인 oauth2 추가

--- src/test/java/wooteco/subway/maps/map/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/maps/map/acceptance/PathAcceptanceTest.java
@@ -1,19 +1,23 @@
 package wooteco.subway.maps.map.acceptance;
 
-import com.google.common.collect.Lists;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static wooteco.subway.maps.line.acceptance.step.LineStationAcceptanceStep.*;
+import static wooteco.subway.maps.map.acceptance.step.PathAcceptanceStep.*;
+import static wooteco.subway.members.favorite.acceptance.FavoriteAcceptanceTest.*;
+import static wooteco.subway.members.member.acceptance.step.MemberAcceptanceStep.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.Lists;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import wooteco.security.core.TokenResponse;
 import wooteco.subway.common.acceptance.AcceptanceTest;
 import wooteco.subway.maps.line.acceptance.step.LineAcceptanceStep;
 import wooteco.subway.maps.line.dto.LineResponse;
 import wooteco.subway.maps.station.acceptance.step.StationAcceptanceStep;
 import wooteco.subway.maps.station.dto.StationResponse;
-
-import static wooteco.subway.maps.line.acceptance.step.LineStationAcceptanceStep.지하철_노선에_지하철역_등록되어_있음;
-import static wooteco.subway.maps.map.acceptance.step.PathAcceptanceStep.*;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
@@ -24,6 +28,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private Long 이호선;
     private Long 신분당선;
     private Long 삼호선;
+
+    private TokenResponse loginResponse;
 
     /**
      * 교대역    --- *2호선* ---   강남역
@@ -55,37 +61,41 @@ public class PathAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철역_등록되어_있음(삼호선, null, 교대역, 0, 0);
         지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 1, 2);
         지하철_노선에_지하철역_등록되어_있음(삼호선, 남부터미널역, 양재역, 2, 2);
+
+        회원_등록되어_있음(EMAIL, PASSWORD, 20);
+        loginResponse = 로그인_되어_있음(EMAIL, PASSWORD);
     }
 
     @DisplayName("두 역의 최단 거리 경로를 조회한다.")
     @Test
     void findPathByDistance() {
         //when
-        ExtractableResponse<Response> response = 거리_경로_조회_요청("DISTANCE", 1L, 3L);
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(loginResponse, "DISTANCE", 1L, 3L);
 
         //then
         적절한_경로를_응답(response, Lists.newArrayList(교대역, 남부터미널역, 양재역));
-        총_거리와_소요_시간과_요금을_함께_응답함(response, 3, 4, 1250d);
+        총_거리와_소요_시간과_요금을_함께_응답함(response, 3, 4, 1750);
     }
-
 
     @DisplayName("두 역의 최소 시간 경로를 조회한다.")
     @Test
     void findPathByDuration() {
         //when
-        ExtractableResponse<Response> response = 거리_경로_조회_요청("DURATION", 1L, 3L);
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(loginResponse, "DURATION", 1L, 3L);
         //then
         적절한_경로를_응답(response, Lists.newArrayList(교대역, 강남역, 양재역));
-        총_거리와_소요_시간과_요금을_함께_응답함(response, 4, 3, 1250d);
+        총_거리와_소요_시간과_요금을_함께_응답함(response, 4, 3, 1750);
     }
 
     private Long 지하철_노선_등록되어_있음(String name, String color) {
-        ExtractableResponse<Response> createLineResponse1 = LineAcceptanceStep.지하철_노선_등록되어_있음(name, color);
+        ExtractableResponse<Response> createLineResponse1 = LineAcceptanceStep.지하철_노선_등록되어_있음(name,
+                color);
         return createLineResponse1.as(LineResponse.class).getId();
     }
 
     private Long 지하철역_등록되어_있음(String name) {
-        ExtractableResponse<Response> createdStationResponse1 = StationAcceptanceStep.지하철역_등록되어_있음(name);
+        ExtractableResponse<Response> createdStationResponse1 = StationAcceptanceStep.지하철역_등록되어_있음(
+                name);
         return createdStationResponse1.as(StationResponse.class).getId();
     }
 }
